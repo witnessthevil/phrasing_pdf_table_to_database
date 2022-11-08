396b39a0cbbd2b0dcd96c228e1b6d03102a59b02
danie <yytamab@connect.ust.hk>
2022-11-09 00:23:15 +0800

    processing population pdf

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,6 +6,10 @@ import java.io.File
 import org.apache.tika.parser.pdf.PDFParser
 import org.apache.tika.parser.ParseContext
 import org.apache.tika.metadata.PDF
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.io.FileNotFoundException
 
 object Main extends App {
   val handler = new BodyContentHandler()
@@ -13,7 +17,7 @@ object Main extends App {
   val metadata = new Metadata()
 
   // create a file stream 
-  val inputstream = new FileInputStream(new File("https://www.censtatd.gov.hk/en/data/stat_report/product/B1120106/att/B11201062021XXXXB01.pdf"))
+  val inputstream = new FileInputStream(new File("/Users/danie/new_project_processing_diff_some_pdf_file/letter-booklet-2020-world-population.pdf"))
   
   // pass context information to tika parser
   val context = new ParseContext()
@@ -21,5 +25,12 @@ object Main extends App {
   val pdfparser = new PDFParser()
   pdfparser.parse(inputstream,handler,metadata,context)
 
-  println(handler.toString())
+  val content = handler.toString()
+  val path = Paths.get("/Users/danie/new_project_processing_diff_some_pdf_file/example.txt")
+  
+  try {
+    Files.write(path,content.getBytes())
+  } catch {
+    case e: FileNotFoundException => println("Couldn't find that file.")
+  }
 }
